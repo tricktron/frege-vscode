a106865942c840c321e48e13113eecc72ef9579d
Thibault Gagnaux <tgagnaux@gmail.com>
2021-08-24 17:08:54 +0200

    feat!: Upgrades frege-lsp-server to `v2.1.8` and adds frege grammar for syntax highlighting

--- test/download-zip.test.ts
+++ test/download-zip.test.ts
@@ -11,11 +11,12 @@ import { it } from 'mocha';
 import * as path from 'path';
 import { getFregeTarGithubUrl, downloadAndExtractTarFromUrl, getFregeStartScriptPath } from '../src/frege-server';
 
+const fregeServerName = 'frege-lsp-server';
+const version = '2.1.8-alpha';
+
 describe('Given frege server github release url', () => {
 
 	it('Then it downloads and extracts the frege server tar', async () => {
-		const fregeServerName = 'frege-lsp-server';
-		const version = '1.0.0-alpha';
 		const url = getFregeTarGithubUrl(fregeServerName, version);
 		const downloadDir = path.join(__dirname, '.frege');
 		await downloadAndExtractTarFromUrl(url, downloadDir);
@@ -25,18 +26,14 @@ describe('Given frege server github release url', () => {
 
 describe('Given frege server name and the frege version', () => {
 	it('Then it can create the path to the frege start script dynamically', () => {
-		const fregeServerName = 'frege-lsp-server';
-		const version = '1.0.0-alpha';
-		let expectedPath = path.normalize('frege-lsp-server-1.0.0-alpha/bin/frege-lsp-server');
+		let expectedPath = path.normalize(`${fregeServerName}-${version}/bin/${fregeServerName}`);
 		if (process.platform === 'win32') {
 			expectedPath = `${expectedPath}.bat`;
 		}
 		assert.strictEqual((getFregeStartScriptPath(fregeServerName, version)), expectedPath);
 	});
 	it('Then it can create the github release url to the frege tar archive dynamically', () => {
-		const fregeServerName = 'frege-lsp-server';
-		const version = '1.0.0-alpha';
-		const expectedDownloadUrl = 'https://github.com/tricktron/frege-lsp-server/releases/download/v1.0.0-alpha/frege-lsp-server-1.0.0-alpha.tar';
+		const expectedDownloadUrl = `https://github.com/tricktron/${fregeServerName}/releases/download/v${version}/${fregeServerName}-${version}.tar`;
 		assert.strictEqual((getFregeTarGithubUrl(fregeServerName, version)), expectedDownloadUrl);
 	});
 });
