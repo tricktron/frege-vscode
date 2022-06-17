8deeb4d844c12737d618f2891ccfab8a99eea9c8
Thibault Gagnaux <tgagnaux@gmail.com>
2022-06-17 17:08:50 +0200

    chore: update to frege language server v3.0.0-alpha

--- test/downloadZip.test.ts
+++ test/downloadZip.test.ts
@@ -11,32 +11,49 @@ import * as path from 'path';
 import { getFregeTarGithubUrl, downloadAndExtractTarFromUrl, getFregeStartScriptPath } from '../src/fregeServer';
 
 const fregeServerName = 'frege-lsp-server';
-const version = '2.1.8-alpha';
+const version = '3.0.0-alpha';
 
-const assertSystemPath = (actual: PathLike, expected: PathLike) => {
-	if (process.platform === 'win32') {
+const assertSystemPath = (actual: PathLike, expected: PathLike) => 
+{
+	if (process.platform === 'win32') 
+    {
 		expected = `${expected}.bat`;
 	}
 	assert.strictEqual(actual, expected);
 }
 
-describe('Given frege server github release url', () => {
-
-	it('Then it downloads and extracts the frege server tar', async () => {
+describe('Given frege server github release url', () => 
+{
+	it('Then it downloads and extracts the frege server tar', async () => 
+    {
 		const url = getFregeTarGithubUrl(fregeServerName, version);
 		const downloadDir = path.join(__dirname, '.frege');
 		await downloadAndExtractTarFromUrl(url, downloadDir);
-		assert.ok(existsSync(path.join(downloadDir, getFregeStartScriptPath(fregeServerName, version))));
+		assert.ok(existsSync(path.join(
+            downloadDir, 
+            getFregeStartScriptPath(fregeServerName, version)
+            ))
+        );
 	}).timeout(10000);
 });
 
-describe('Given frege server name and the frege version', () => {
-	it('Then it can create the path to the frege start script dynamically', () => {
-		const expectedStartScriptPath = path.normalize(`${fregeServerName}-${version}/bin/${fregeServerName}`);
-		assertSystemPath((getFregeStartScriptPath(fregeServerName, version)), expectedStartScriptPath);
+describe('Given frege server name and the frege version', () => 
+{
+	it('Then it can create the path to the frege start script dynamically', () => 
+    {
+		const expectedStartScriptPath = 
+            path.normalize(`${fregeServerName}-${version}/bin/${fregeServerName}`);
+		assertSystemPath(
+            getFregeStartScriptPath(fregeServerName, version), 
+            expectedStartScriptPath
+        );
 	});
-	it('Then it can create the github release url to the frege tar archive dynamically', () => {
-		const expectedDownloadUrl = `https://github.com/tricktron/${fregeServerName}/releases/download/v${version}/${fregeServerName}-${version}.tar`;
-		assert.strictEqual((getFregeTarGithubUrl(fregeServerName, version)), expectedDownloadUrl);
+	it('Then it can create the github release url to the frege tar archive dynamically', () => 
+    {
+		const expectedDownloadUrl = 
+            `https://github.com/tricktron/${fregeServerName}/releases/download/v${version}/${fregeServerName}-${version}.tar`;
+		assert.strictEqual(
+            getFregeTarGithubUrl(fregeServerName, version), 
+            expectedDownloadUrl);
 	});
 });
